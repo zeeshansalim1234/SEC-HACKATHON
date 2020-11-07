195ce979ef7b916f4f6fc04ddf97eab69e69f5d4
zeeshansalim1234 <59512700+zeeshansalim1234@users.noreply.github.com>
2020-11-07 11:13:25 -0700

    Create class.py

---
+++ class.py
@@ -0,0 +1,15 @@
+class user_info:  #class for managing all user info
+
+    def __init__(self,name,email,password,dob,loc,bio,pic,school,work,interest,post):
+
+        self.name=name
+        self.email=email
+        self.password=password
+        self.dob=dob
+        self.loc=loc
+        self.bio=bio
+        self.pic=pic
+        self.school=school
+        self.work=work
+        self.interest=interest
+        self.post=post
