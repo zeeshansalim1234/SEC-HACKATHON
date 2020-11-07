b341df4761b2c978ac3afb278494f46de78c928b
zeeshansalim1234 <59512700+zeeshansalim1234@users.noreply.github.com>
2020-11-07 12:50:06 -0700

    Update class.py

--- class.py
+++ class.py
@@ -1,6 +1,10 @@
+no_of_users=0
 class user_info:  #class for managing all user info
 
-    def __init__(self,name,email,password,dob,loc,bio,pic,school,work,interest,post):
+
+    sums=[]
+    friends_preferences=[]
+    def __init__(self,name,email,password,dob,loc,bio,pic,school,work,interest,post,friends):
 
         self.name=name
         self.email=email
@@ -13,3 +17,34 @@ class user_info:  #class for managing all user info
         self.work=work
         self.interest=interest
         self.post=post
+        self.friends=friends
+
+
+arr=[]
+arr.append(user_info(1,2,3,4,5,6,7,8,9,1,5,2))
+arr.append(user_info(1,2,3,4,5,6,7,8,9,1,5,2))
+print(len(arr))
+for i in range(0,len(arr)):
+
+    for j in range(0,len(arr)):
+
+        if(i!=j):
+
+            if(arr[i].loc==arr[j].loc):
+                arr[i].sums[j]+=20
+
+            if(arr[i].interest==arr[j].interest):
+                arr[i].sums[j]+=10
+
+            elif((arr[i].interest in arr[j].interest )or(arr[j].interest in arr[i].interest)):
+                arr[i].sums[j]+=5
+
+            list_set=set(arr[i].friends)
+            intersection= list_set.intersection(arr[j].friends)
+            intersection_list=list(intersection)
+            arr[i].sums[j]+=10*(len(intersection_list))
+
+    arr[i].friends_preferences=[i[0] for i in sorted(enumerate(arr[i].sums), key=lambda k: k[1], reverse=True)]
+
+
+
